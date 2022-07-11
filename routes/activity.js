dcdf09862585eb12bfa5bbb965761707ee47f586
anmol09988 <107464613+anmol09988@users.noreply.github.com>
2022-07-11 15:45:24 +0530

    updated

--- routes/activity.js
+++ routes/activity.js
@@ -114,14 +114,14 @@ exports.execute = function (req, res) {
 
     const userAction = async () => {
         console.log("check for of the integration");
-        const response = await fetch('http://example.com/movies.json', {
-          method: 'POST',
-          body: myBody, // string or object
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        });
-        const myJson = await response.json(); //extract JSON from the http response
+        // const response = await fetch('http://example.com/movies.json', {
+        //   method: 'POST',
+        //   body: myBody, // string or object
+        //   headers: {
+        //     'Content-Type': 'application/json'
+        //   }
+        // });
+        // const myJson = await response.json(); //extract JSON from the http response
         // do something with myJson
       }
 
